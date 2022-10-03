3c6082f012f2228a2264f8c74136d1f083e0b8ed
aderemilekun <adelefatai@yahoo.com>
2022-10-03 18:03:48 +0100

    Search repository test passed

---
+++ test/features/data/repository/search_repository_test.dart
@@ -0,0 +1,74 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:youtube_data_api/core/network_info.dart';
+import 'package:youtube_data_api/features/data/datasources/get_search_remote_datasource.dart';
+import 'package:youtube_data_api/features/data/model/videos_response.dart';
+import 'package:youtube_data_api/features/data/repository/search_repository_implementation.dart';
+
+class MockNetworkInfo extends Mock implements NetworkInfo {}
+
+class MockGetVideosRemoteDataSource extends Mock
+    implements SearchRemoteDatasource {}
+
+void main() {
+  String query = "flutter";
+
+  late MockGetVideosRemoteDataSource mockRemoteDataSource;
+  late MockNetworkInfo mockNetworkInfo;
+  late SearchRepositoryImpl repository;
+
+  mockRemoteDataSource = MockGetVideosRemoteDataSource();
+  mockNetworkInfo = MockNetworkInfo();
+  repository = SearchRepositoryImpl(
+       mockRemoteDataSource, mockNetworkInfo);
+
+  void runTestsOnline(Function body) {
+    group('device is online', () {
+      setUp(() {
+        when(mockNetworkInfo.isConnected).thenAnswer((_) async => true);
+      });
+
+      body();
+    });
+  }
+
+  group("Get Search", () {
+    final testSearchVideosModel = VideosResponse(
+        kind: "youtube#searchListResponse",
+        etag: "BcN2E3k2QTV5SGz-XeH8xDq7IKY",
+        regionCode: "US",
+        nextPageToken: "CAUQAA",
+        pageInfo: PageInfo(totalResults: 5774, resultsPerPage: 5),
+        items:  const []);
+
+    test(
+      'should check if the device is online',
+          () async {
+        // arrange
+        when(mockNetworkInfo.isConnected).thenAnswer((_) async => true);
+        // act
+        await repository.getSearch(query);
+        // assert
+        verify(mockNetworkInfo.isConnected);
+      },
+    );
+
+    runTestsOnline(() {
+      test(
+        'should return remote data when the call is successful',
+            () async {
+          // arrange
+          when(mockRemoteDataSource.getSearch(query))
+              .thenAnswer((_) async => testSearchVideosModel);
+          // act
+          final result = await repository.getSearch(query);
+          // assert
+          verify(mockRemoteDataSource.getSearch(query));
+
+          expect(result, equals(Right(testSearchVideosModel)));
+        },
+      );
+    });
+  });
+}
