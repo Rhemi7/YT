183575bc4d8f57100452f839d99890ec88c57c63
aderemilekun <adelefatai@yahoo.com>
2022-10-03 17:58:01 +0100

    Search remote datasource test passed

--- test/features/data/datasource/get_search_remote_datasource_test.dart
+++ test/features/data/datasource/get_search_remote_datasource_test.dart
@@ -3,7 +3,7 @@ import 'package:mockito/mockito.dart';
 import 'package:http/http.dart' as http;
 import 'package:youtube_data_api/constants/const.dart';
 import 'package:youtube_data_api/core/error/failure.dart';
-import 'package:youtube_data_api/features/data/datasources/get_videos_remote_datasource.dart';
+import 'package:youtube_data_api/features/data/datasources/get_search_remote_datasource.dart';
 import 'package:youtube_data_api/features/data/model/videos_response.dart';
 
 import '../../../data/data_reader.dart';
@@ -13,31 +13,31 @@ class MockClient extends Mock implements http.Client {}
 void main() {
 
   late MockClient client;
-  late GetVideosRemoteDatasourceImpl dataSource;
+  late SearchRemoteDatasourceImpl dataSource;
+  String query = "Flutter";
+  var url = Uri.parse("$baseUrl/search?channelId=$channelID&part=snippet&q=$query&maxResults=10&type=video&key=$apIkey");
 
-  var url = Uri.parse('$baseUrl/search?channelId=$channelID&part=snippet&order=date&maxResults=10&key=$apIkey');
   setUp(() {
     client = MockClient();
-    dataSource = GetVideosRemoteDatasourceImpl (client);
+    dataSource = SearchRemoteDatasourceImpl(client);
   });
 
-  group('get videos', () {
-    test('returns a video response if the http call completes successfully',
+  group('get search', () {
+    test('returns a video response of the search query if the http call completes successfully',
             () async {
           when(client.get(url, headers: headers)).thenAnswer(
                   (_) async => http.Response(dataReader('videos_response.json'), 200));
 
-          expect(await dataSource.getVideos(), isA<VideosResponse>());
+          expect(await dataSource.getSearch(query), isA<VideosResponse>());
         });
 
     test('throws an exception if the http call results in an error', () {
       when(client.get(url, headers: headers))
           .thenAnswer((_) async => http.Response('Not Found', 404));
 
-      expect(() => dataSource.getVideos(),
+      expect(() => dataSource.getSearch(query),
           throwsA(const TypeMatcher<ServerException>()));
     });
   });
 
 }
-
