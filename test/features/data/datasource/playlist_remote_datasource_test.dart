d07738466f9bf536de876e7204d4483739eb3cdc
aderemilekun <adelefatai@yahoo.com>
2022-10-04 15:55:51 +0100

    Unit test for get next playlist in playlist repository passed

--- test/features/data/datasource/playlist_remote_datasource_test.dart
+++ test/features/data/datasource/playlist_remote_datasource_test.dart
@@ -17,8 +17,12 @@ void main() {
 
   String playlistId = "PLOU2XLYxmsIIjHK9mEgLey-CaFcajdUBX";
 
+  String pageToken = "CBKQAA";
+
   var playlistUrl = Uri.parse(
       '$baseUrl/playlists?channelId=$channelID&key=$apIkey&part=snippet,contentDetails&maxResults=10');
+
+  var nextPlaylist = Uri.parse('$baseUrl/playlists?channelId=$channelID&key=$apIkey&part=snippet,contentDetails&maxResults=10&pageToken=$pageToken');
 
   var playlistVidUrl = Uri.parse(
       '$baseUrl/playlistItems?playlistId=$playlistId&key=$apIkey&part=snippet');
@@ -62,6 +66,24 @@ void main() {
           .thenAnswer((_) async => http.Response('Not Found', 404));
 
       expect(() => dataSource.getPlayListVideos(playlistId),
+          throwsA(const TypeMatcher<ServerException>()));
+    });
+  });
+
+  group('Get Next Set of Playlist', () {
+    test('returns a playlist response if the http call completes successfully',
+            () async {
+          when(client.get(nextPlaylist, headers: headers)).thenAnswer((_) async =>
+              http.Response(dataReader('playlist_response.json'), 200));
+
+          expect(await dataSource.getNextPlaylist(pageToken), isA<PlaylistResponse>());
+        });
+
+    test('throws an exception if the http call results in an error', () {
+      when(client.get(nextPlaylist, headers: headers))
+          .thenAnswer((_) async => http.Response('Not Found', 404));
+
+      expect(() => dataSource.getNextPlaylist(pageToken),
           throwsA(const TypeMatcher<ServerException>()));
     });
   });
