314090bc01d65b688dfdb7edbeee0676726550cd
aderemilekun <adelefatai@yahoo.com>
2022-10-04 16:49:08 +0100

    filtering videos according to date, rating and views

---
+++ lib/core/filter_enum.dart
@@ -0,0 +1,1 @@
+enum FilterType { date, views, rating }
--- lib/features/presentation/screens/home_screen.dart
+++ lib/features/presentation/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flag/flag.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:youtube_data_api/core/filter_enum.dart';
 import 'package:youtube_data_api/features/presentation/notifier/get_channel/get_channel_state.dart';
 import 'package:youtube_data_api/features/presentation/notifier/get_playlist/get_playlist_state.dart';
 import 'package:youtube_data_api/features/presentation/notifier/get_videos/get_videos_state.dart';
@@ -312,53 +313,140 @@ class _VideosTabViewState extends ConsumerState<VideosTabView> {
     super.dispose();
   }
 
+  FilterType filterValue = FilterType.date;
+
   @override
   Widget build(BuildContext context) {
-    return Consumer(
-        builder: (BuildContext context, WidgetRef ref, Widget? child) {
-      final state = ref.watch(getVideosNotifierProvider);
-      if (state is GetVideosLoading) {
-        return const Center(
-          child: CircularProgressIndicator(),
-        );
-      } else if (state is GetVideosLoaded) {
-        return ListView.builder(
-          itemCount: state.items!.length,
-          controller: videosTabController,
-          itemBuilder: (BuildContext context, int i) {
-            if (i == state.items!.length - 1) {
-              return const Padding(
-                padding: EdgeInsets.all(8.0),
-                child: SizedBox(
-                    height: 40,
-                    width: 40,
-                    child: Center(child: CircularProgressIndicator())),
+    return Column(
+      children: [
+        YMargin(15),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FilterWidget(
+                text: "Date",
+                buttonColor: filterValue == FilterType.date
+                    ? Colors.red
+                    : Colors.grey,
+                onTap: () {
+                  if (filterValue != FilterType.date) {
+                    filterValue = FilterType.date;
+
+                    ref
+                        .watch(getVideosNotifierProvider.notifier)
+                        .getChannelVideos('date');
+                    setState(() {});
+                  }
+                }),
+            const XMargin(15),
+            FilterWidget(
+                text: "Views",
+                buttonColor: filterValue == FilterType.views
+                    ? Colors.red
+                    : Colors.grey,
+                onTap: () {
+                  if (filterValue != FilterType.views) {
+                    filterValue = FilterType.views;
+                    ref
+                        .watch(getVideosNotifierProvider.notifier)
+                        .getChannelVideos('viewCount');
+                    setState(() {});
+                  }
+                }),
+            const XMargin(15),
+            FilterWidget(
+                text: "Ratings",
+                buttonColor: filterValue == FilterType.rating
+                    ? Colors.red
+                    : Colors.grey,
+                onTap: () {
+                  if (filterValue != FilterType.rating) {
+                    filterValue = FilterType.rating;
+
+                    ref
+                        .watch(getVideosNotifierProvider.notifier)
+                        .getChannelVideos('rating');
+                    setState(() {});
+                  }
+                }),
+          ],
+        ),
+        Expanded(
+          child: Consumer(
+              builder: (BuildContext context, WidgetRef ref, Widget? child) {
+            final state = ref.watch(getVideosNotifierProvider);
+            if (state is GetVideosLoading) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (state is GetVideosLoaded) {
+              return ListView.builder(
+                itemCount: state.items!.length,
+                controller: videosTabController,
+                itemBuilder: (BuildContext context, int i) {
+                  if (i == state.items!.length - 1) {
+                    return const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: SizedBox(
+                          height: 40,
+                          width: 40,
+                          child: Center(child: CircularProgressIndicator())),
+                    );
+                  }
+                  return VideoTileWidget(
+                    video: state.items![i],
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => PlayVideoScreen(
+                                  videoId:
+                                      state.items![i].id!.videoId.toString())));
+                    },
+                  );
+                },
+              );
+            } else if (state is GetVideosError) {
+              return AppErrorWidget(
+                error: state.message,
+                onTap: () {
+                  ref
+                      .watch(getVideosNotifierProvider.notifier)
+                      .getChannelVideos('date');
+                },
               );
             }
-            return VideoTileWidget(
-              video: state.items![i],
-              onTap: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => PlayVideoScreen(
-                            videoId: state.items![i].id!.videoId.toString())));
-              },
-            );
-          },
-        );
-      } else if (state is GetVideosError) {
-        return AppErrorWidget(
-          error: state.message,
-          onTap: () {
-            ref
-                .watch(getVideosNotifierProvider.notifier)
-                .getChannelVideos('date');
-          },
-        );
-      }
-      return const SizedBox.shrink();
-    });
+            return const SizedBox.shrink();
+          }),
+        ),
+      ],
+    );
+  }
+}
+
+class FilterWidget extends StatelessWidget {
+  final String text;
+  final void Function()? onTap;
+  final Color? buttonColor;
+  const FilterWidget({
+    Key? key,
+    required this.text,
+    this.buttonColor,
+    this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        height: 30,
+        width: 60,
+        decoration: BoxDecoration(
+            color: buttonColor, borderRadius: BorderRadius.circular(10)),
+        child: Center(child: Text(text)),
+      ),
+    );
   }
 }
 
