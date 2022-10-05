6e4957a171b3ac3f7f0ec898e2d89010c96028c9
aderemilekun <adelefatai@yahoo.com>
2022-10-05 09:39:12 +0100

    Get Next Playlist Videos Test in Datasource Passed

--- test/features/data/datasource/playlist_remote_datasource_test.dart
+++ test/features/data/datasource/playlist_remote_datasource_test.dart
@@ -19,6 +19,8 @@ void main() {
 
   String pageToken = "CBKQAA";
 
+  String nextPlaylistVidToken = "EAAaBlBUOkNBVQ";
+
   var playlistUrl = Uri.parse(
       '$baseUrl/playlists?channelId=$channelID&key=$apIkey&part=snippet,contentDetails&maxResults=10');
 
@@ -26,6 +28,9 @@ void main() {
 
   var playlistVidUrl = Uri.parse(
       '$baseUrl/playlistItems?playlistId=$playlistId&key=$apIkey&part=snippet&maxResults=10');
+
+  var nextPlaylistVideos = Uri.parse(
+      '$baseUrl/playlistItems?pageToken=$nextPlaylistVidToken&playlistId=$playlistId&key=$apIkey&part=snippet&maxResults=10');
 
   setUp(() {
     client = MockClient();
@@ -84,6 +89,25 @@ void main() {
           .thenAnswer((_) async => http.Response('Not Found', 404));
 
       expect(() => dataSource.getNextPlaylist(pageToken),
+          throwsA(const TypeMatcher<ServerException>()));
+    });
+  });
+
+
+  group('Get Next Set of Playlist Videos', () {
+    test('returns a playlist video response if the http call completes successfully',
+            () async {
+          when(client.get(nextPlaylistVideos, headers: headers)).thenAnswer((_) async =>
+              http.Response(dataReader('playlist_videos_response.json'), 200));
+
+          expect(await dataSource.getNextPlayListVideos(pageToken: nextPlaylistVidToken, playlistID: playlistId), isA<PlaylistVideoResponse>());
+        });
+
+    test('throws an exception if the http call results in an error', () {
+      when(client.get(nextPlaylistVideos, headers: headers))
+          .thenAnswer((_) async => http.Response('Not Found', 404));
+
+      expect(() => dataSource.getNextPlayListVideos(pageToken: nextPlaylistVidToken, playlistID: playlistId),
           throwsA(const TypeMatcher<ServerException>()));
     });
   });
